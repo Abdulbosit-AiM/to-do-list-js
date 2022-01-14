61500767da028661d90c6cbbe01f3345c74fa974
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-01-14 22:44:54 +0500

    change select bg

--- script.js
+++ script.js
@@ -66,14 +66,14 @@ function filterTodo(e){
     todos.forEach(function(todo){
         switch(e.target.value){
             case 'all':
-                todo.style.display = 'flex';
-                break
+                todo.style.display = 'flex'
+                break;
             case 'completed':
                 if(todo.classList.contains('completed')){
                     todo.style.display = 'flex'
                 }else {
                     todo.style.display = 'none'
-                } break
+                } break;
             case 'uncompleted':
                 if(!todo.classList.contains('completed')){
                     todo.style.display = 'flex'
