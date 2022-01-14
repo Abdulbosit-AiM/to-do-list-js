c00b40caa4d6282ed0ca805ce85c05794e3de796
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-01-14 18:27:09 +0500

    minor improvements

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
 const filterOption = document.querySelector('.filter-todo')
 
-
 // Event Listeners
-
+// document.addEventListener('DOMContentLoaded', getTodos)  
 todoButton.addEventListener('click', addTodo)
 todoList.addEventListener('click', deleteCheck)
 filterOption.addEventListener('click', filterTodo)
-
 
 // Functions
 
@@ -63,11 +61,12 @@ function deleteCheck(e) {
 }
 
 function filterTodo(e){
+    
     const todos = todoList.childNodes
     todos.forEach(function(todo){
         switch(e.target.value){
             case 'all':
-                todo.style.display = 'flex'
+                todo.style.display = 'flex';
                 break
             case 'completed':
                 if(todo.classList.contains('completed')){
@@ -80,7 +79,7 @@ function filterTodo(e){
                     todo.style.display = 'flex'
                 }else {
                     todo.style.display = 'none'
-                } break
+                }
         }
     })
 }
